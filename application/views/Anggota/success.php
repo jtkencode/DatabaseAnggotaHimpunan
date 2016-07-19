5f2bdee97d579868bf2d8df5bd08e6664647b569
Imam Fauzan <Imam Fauzan>
2016-07-19 19:11:41 +0700

    add success template after edit data

---
+++ application/views/Anggota/success.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<html>
+<head>
+	<title>Data Anggota Himakom</title>
+	<link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet"> 
+	<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
+</head>
+<body>
+	<div class="container">
+			
+	</div>
+
+	
+</body>
+</html>
