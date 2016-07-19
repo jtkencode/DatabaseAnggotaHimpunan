7f157df9431169ee590a95ffe8a03249f813b318
Imam Fauzan <Imam Fauzan>
2016-07-19 19:18:56 +0700

    edit success page

--- application/views/Anggota/success.php
+++ application/views/Anggota/success.php
@@ -10,7 +10,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 	<div class="container">
-			
+		<div class="page-header">
+			</div>
+			<div class="row">
+				<div class="col-sm-offset-3 col-sm-6">
+					<div class="alert alert-success" role="alert">
+						<h2 align="center"><strong>Data berhasil dimasukkan!</strong></h2>
+						<h4 align="center">Terima kasih telah berkontribusi untuk Himakom:)</h4>
+						Silahkan memeriksa data yang sudah masuk pada <a href="../anggota/cekdata.php?nim=<?php echo $nim;?>">dashboard anda</a> untuk memastikan data yang kamu masukkan sesuai. Mohon maaf atas ketidaknyamanan sistem kami yang masih belum sempurna :). <br><br>
+						<div class="text-center"> 
+							<a href="../dashboard" class="btn btn-success" align="center">Kembali ke Dashboard </a>
+						</div>
+					</div>
+				</div>
+			</div><!-- end row -->		
 	</div>
 
 	
