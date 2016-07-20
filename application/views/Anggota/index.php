23a2b2dc1e8fd65557b2d4790c3930de4e5d6e49
Imam Fauzan <imamfzn@gmail.com>
2016-07-20 18:43:16 +0700

    delete crud js

--- application/views/Anggota/index.php
+++ application/views/Anggota/index.php
@@ -8,7 +8,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet"> 
 	<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
 	<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
-	<script src="<?php echo base_url('assets/js/crud.js'); ?>"></script>
 </head>
 <body>
 	<div class="container">
