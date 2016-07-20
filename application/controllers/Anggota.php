83a9ddaab5b1f9720d586788be6e800caf73e261
Imam Fauzan <imamfzn@gmail.com>
2016-07-20 17:00:57 +0700

    add return function on add_contatct

--- application/controllers/Anggota.php
+++ application/controllers/Anggota.php
@@ -72,15 +72,14 @@ class Anggota extends CI_Controller {
 		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 			$this->load->view('anggota/add_contact',$data);
 		} else {
-			$update = $this->Anggota_Model->change_password($nim);
 			$data_update = array(
 							'NIM' => $nim,
 							'DETIL_KONTAK' => $this->input->post('detil_kontak');
 							'JENIS_KONTAK' => $this->input->post('jenis_kontak');
 						);
-
+			$update = $this->Anggota_Model->add_contact($data_update);
 			if ($update){
-				redirect('anggota/success');
+				return $update;
 			} else echo "Update Gagal";
 		}	
 	}
