1d392d0511eaa49bbe05af8ec497bcfa9ca4c184
Imam Fauzan <imamfzn@gmail.com>
2016-07-20 13:33:45 +0700

    add javascript function to show modal for add contact

--- application/views/Anggota/index.php
+++ application/views/Anggota/index.php
@@ -7,6 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>Data Anggota Himakom</title>
 	<link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet"> 
 	<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 	<script src="<?php echo base_url('assets/js/crud.js'); ?>"></script>
 </head>
 <body>
---
+++ assets/js/crud.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('#btnAddContact').click(function(){
+		$('#contactModal').modal('show'); 
+	});
+});
