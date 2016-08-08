8f621a65016413670e69a774da4cb7978a8726e3
Imam Fauzan <imamfzn@gmail.com>
2016-08-08 11:43:56 +0700

    fix funtion on riwayat prestasi

--- application/controllers/Anggota.php
+++ application/controllers/Anggota.php
@@ -34,7 +34,7 @@ class Anggota extends CI_Controller {
 		$data['riwayat_pendidikan'] = $this->Riwayat_Pendidikan_model->get_nim($nim);
 		$data['riwayat_org'] = $this->Riwayat_Org_model->get_nim($nim);
 		$data['tingkat_prestasi'] = $this->Tingkat_Prestasi_model->get_all();
-		$data['riwayat_prestasi'] = $this->Riwayat_Prestasi_model->get_id($nim);
+		$data['riwayat_prestasi'] = $this->Riwayat_Prestasi_model->get_nim($nim);
 		$data['riwayat_kepanitiaan'] = $this->Riwayat_Kepanitiaan_model->get_nim($nim);
 		$data['riwayat_pelatihan'] = $this->Riwayat_Pelatihan_model->get_id($nim);
 		$data['riwayat_pkm'] = $this->Riwayat_PKM_model->get_nim($nim);
@@ -132,7 +132,7 @@ class Anggota extends CI_Controller {
 		} else {
 			$insert = $this->Riwayat_Prestasi_model->add_riwayat_prestasi($data['nim']);
 			if ($insert){
-				 echo json_encode(array("status" => TRUE));
+				redirect('anggota/success');
 			} else echo "Insert Gagal";
 		}	
 	}
--- application/models/Riwayat_Prestasi_model.php
+++ application/models/Riwayat_Prestasi_model.php
@@ -8,12 +8,20 @@ class Riwayat_Prestasi_model extends CI_Model{
 		parent::__construct();
 	}
 
-	public function get_id($id)
+	public function get_nim($nim)
 	{
-		$query = $this->db->where('nim', $id)->get('riwayat_prestasi');
+		$query = $this->db->where('nim', $nim)->get('riwayat_prestasi');
 		$result = $query->result();
 
 		return $result;
+	}
+
+	public function get_id($id)
+	{
+		$query = $this->db->where('no_urut_prestasi', $id)->get('riwayat_prestasi');
+		$result = $query->result();
+
+		return $result[0];
 	}
 
 	public function get_last_no($id)
@@ -48,6 +56,30 @@ class Riwayat_Prestasi_model extends CI_Model{
 		$query = $this->db->insert('riwayat_prestasi',$data);
 		return $query;
 	}
+
+	public function update_riwayat_prestasi($id)
+	{	
+		$data = array(
+				'ID_TINGKAT_PRESTASI' => $this->input->post('tingkat_prestasi'),
+				'NAMA_PRESTASI' => $this->input->post('nama_prestasi'),
+				'PENCAPAIAN_PRESTASI' => $this->input->post('pencapaian_prestasi'),
+				'LEMBAGA_PRESTASI' => $this->input->post('lembaga_prestasi'),
+				'TAHUN_PRESTASI' => $this->input->post('tahun_prestasi'),
+				'JENIS_PRESTASI' => $this->input->post('jenis_prestasi')
+			);
+
+		$this->db->where('NO_URUT_PRESTASI',$id);
+		$query = $this->db->update('riwayat_prestasi',$data);
+		return $query;
+	}
+
+	public function delete_riwayat_prestasi($id)
+	{
+		$this->db->where('NO_URUT_PRESTASI',$id);
+		$query = $this->db->delete('riwayat_prestasi');
+		return $query;
+	}
+
 
 }
 
