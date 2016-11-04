e59742175fe8117a7bbf2da47e0126f980d3a769
Imam Fauzan <imamfzn@gmail.com>
2016-11-05 00:02:02 +0700

    make riwayat pkm nav active

--- application/views/Anggota/riwayat_pkm.php
+++ application/views/Anggota/riwayat_pkm.php
@@ -30,8 +30,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							  <li><a href="<?php echo site_url('anggota/riwayat_organisasi') ;?>">Organisasi</a></li>
 							  <li><a href="<?php echo site_url('anggota/riwayat_prestasi') ;?>">Prestasi</a></li>
 							  <li><a href="<?php echo site_url('anggota/riwayat_kepanitiaan') ;?>">Kepanitiaan</a></li>
-							  <li class="active"><a href="<?php echo site_url('anggota/riwayat_pelatihan') ;?>">Pelatihan</a></li>
-							  <li ><a href="<?php echo site_url('anggota/riwayat_pkm') ;?>">PKM</a></li>
+							  <li><a href="<?php echo site_url('anggota/riwayat_pelatihan') ;?>">Pelatihan</a></li>
+							  <li class="active"><a href="<?php echo site_url('anggota/riwayat_pkm') ;?>">PKM</a></li>
 							</ul>
 						</div>
 						<div class="row">
