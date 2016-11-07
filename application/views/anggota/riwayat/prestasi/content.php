e0891e3d2556b4ff92b6447c3fefd437c8726008
Imam Fauzan <imamfzn@gmail.com>
2016-11-07 23:23:30 +0700

    add edit button

--- application/views/anggota/riwayat/prestasi/content.php
+++ application/views/anggota/riwayat/prestasi/content.php
@@ -13,6 +13,11 @@
 											<p><?php echo $row->NAMA_PRESTASI;?> <small><?php echo $row->PENCAPAIAN_PRESTASI;?></small></p>
 											<p><small><?php echo $row->LEMBAGA_PRESTASI;?></small></p>
 											<p class="text-right"><span class="glyphicon glyphicon-time"></span><small> <?php echo $row->TAHUN_PRESTASI;?></small></p>
+											<!-- Button for edit riwayat-->
+											<div class="hiddenButton" hidden>
+												<a href="<?php echo site_url('anggota/riwayat_prestasi/update'),'/',$row->NO_URUT_PRESTASI;?>" class="btn btn-success btn-sm" title="ubah data"><span class="glyphicon glyphicon-pencil"></span></a>
+												<a href="<?php echo site_url('anggota/riwayat_prestasi/delete'),'/',$row->NO_URUT_PRESTASI;?>" class="btn btn-danger btn-sm" title="hapus data"><span class="glyphicon glyphicon-trash"></span></a>
+											</div>
 										</div>
 									</div>
 								</div>
