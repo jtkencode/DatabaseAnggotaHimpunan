7b88e480eb04eb3e7e7fc4cf67725ae451029302
Imam Fauzan <imamfzn@gmail.com>
2016-11-07 23:19:40 +0700

    add edit button

--- application/views/anggota/riwayat/pelatihan/content.php
+++ application/views/anggota/riwayat/pelatihan/content.php
@@ -13,6 +13,11 @@
 											<p><?php echo $row->NAMA_PELATIHAN;?></p>
 											<p><small><?php echo $row->NAMA_PENYELENGGARA_PELATIHAN;?></small></p>
 											<p class="text-right"><span class="glyphicon glyphicon-time"></span><small> <?php echo $row->TAHUN_PELATIHAN?></small></p>
+											<!-- Button for edit riwayat-->
+											<div class="hiddenButton" hidden>
+												<a href="<?php echo site_url('anggota/riwayat_pelatihan/update'),'/',$row->NO_URUT_PELATIHAN;?>" class="btn btn-success btn-sm" title="ubah data"><span class="glyphicon glyphicon-pencil"></span></a>
+												<a href="<?php echo site_url('anggota/riwayat_pelatihan/delete'),'/',$row->NO_URUT_PELATIHAN;?>" class="btn btn-danger btn-sm" title="hapus data"><span class="glyphicon glyphicon-trash"></span></a>
+											</div>
 										</div>
 									</div>
 								</div>
