f8405162b22ba874b7ceb544427fb4674c81d9df
Imam Fauzan <imamfzn@gmail.com>
2016-11-07 20:12:30 +0700

    add riwayat pkm controller

---
+++ application/controllers/anggota/Riwayat_PKM.php
@@ -0,0 +1,84 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Riwayat_PKM extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();		
+		
+		if(empty($this->session->userdata('user'))) {
+            redirect('login');
+        }
+
+		$this->load->model('Anggota_Model');
+		$this->load->model('Kontak_model');
+		$this->load->model('Riwayat_PKM_model');
+	}
+
+	public function riwayat_pkm()
+	{
+		$nim = $this->session->userdata('user');
+		$data['anggota'] = $this->Anggota_Model->get_id($nim);
+		$data['kontak'] = $this->Kontak_model->get_id($nim);
+		$data['riwayat_pkm'] = $this->Riwayat_PKM_model->get_nim($nim);
+		$this->load->view('anggota/riwayat/pkm/riwayat_pkm',$data);
+	}
+
+	public function add()
+	{ 
+		$data['nim'] = $this->session->userdata('user');
+		
+		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+			$this->load->view('anggota/add_riwayat_pkm',$data);
+		} else {
+			$insert = $this->Riwayat_PKM_model->add_riwayat_pkm($data['nim']);
+			if ($insert){
+				 redirect('anggota/success');
+			} else echo "Insert Gagal";
+		}	
+	}
+
+	public function get($id = null)
+	{
+		if ($id != null) {
+			$data = $this->Riwayat_PKM_model->get_id($id);
+		} else {
+			$nim = $this->session->userdata('user');
+			$data = $this->Riwayat_PKM_model->get_nim($nim);
+		}
+		
+		return $data;
+	}
+
+	public function update($id)
+	{ 
+		$data['nim'] = $this->session->userdata('user');
+		
+		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+			$data['riwayat_pkm']= $this->get_riwayat_pkm($id);
+			$this->load->view('anggota/update_riwayat_pkm',$data);
+		} else {
+			$update = $this->Riwayat_PKM_model->update_riwayat_pkm($id);
+			if ($update){
+				  redirect('anggota/success');
+			} else echo "Update Gagal";
+		}	
+	}
+
+	public function delete($id)
+	{ 
+		$data['nim'] = $this->session->userdata('user');
+		
+		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+			$data['riwayat_pkm']= $this->get_riwayat_pkm($id);
+			$this->load->view('anggota/delete_riwayat_pkm',$data);
+		} else {
+			$delete = $this->Riwayat_PKM_model->delete_riwayat_pkm($id);
+			if ($delete){
+				 redirect('anggota/success');
+			} else echo "Delete Gagal";
+		}	
+	}
+
+}
