a6b4419be3e46d3ec125460a79d609dc100afc50
Imam Fauzan <imamfzn@gmail.com>
2016-11-07 23:26:51 +0700

    ADD EDIT BUTTON

--- application/views/anggota/riwayat/kepanitiaan/content.php
+++ application/views/anggota/riwayat/kepanitiaan/content.php
@@ -13,6 +13,11 @@
 											<p><?php echo $row->NAMA_KEGIATAN_KEPANITIAAN;?> <small><?php echo $row->JABATAN_KEPANITIAAN;?></small></p>
 											<p><small><?php echo $row->NAMA_ORG_KEPANITIAAN;?></small></p>
 											<p class="text-right"><span class="glyphicon glyphicon-time"></span><small> <?php echo $row->TAHUN_KEPANITIAAN;?></small></p>
+											<!-- Button for edit riwayat-->
+											<div class="hiddenButton" hidden>
+												<a href="<?php echo site_url('anggota/riwayat_kepanitiaan/update'),'/',$row->NO_URUT_KEPANITIAAN;?>" class="btn btn-success btn-sm" title="ubah data"><span class="glyphicon glyphicon-pencil"></span></a>
+												<a href="<?php echo site_url('anggota/riwayat_kepanitiaan/delete'),'/',$row->NO_URUT_KEPANITIAAN;?>" class="btn btn-danger btn-sm" title="hapus data"><span class="glyphicon glyphicon-trash"></span></a>
+											</div>
 										</div>
 									</div>
 								</div>
