af81ff5efbf352251142a55184f260f81c41d96c
Imam Fauzan <imamfzn@gmail.com>
2016-11-07 20:16:08 +0700

    change controller name

--- application/controllers/anggota/Riwayat_Organisasi.php
+++ application/controllers/anggota/Riwayat_Organisasi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Riwayat_Org extends CI_Controller {
+class Riwayat_Organisasi extends CI_Controller {
 
 	public function __construct()
 	{
