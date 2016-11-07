a5b5256a618092df0bcc5f7521cd765156dc9008
Imam Fauzan <imamfzn@gmail.com>
2016-11-07 20:34:45 +0700

    pisah view

---
+++ application/views/anggota/footer.php
@@ -0,0 +1,8 @@
+<footer>
+					<hr> <p class="text-center">&copy; 2016 Himpunan Mahasiswa Komputer Politeknik Negeri Bandung</p>
+				</footer>
+			</div>	
+		</div>		
+	</div>
+</body>
+</html>
---
+++ application/views/anggota/nav_riwayat.php
@@ -0,0 +1,14 @@
+					<!--Tabs Riwayat -->
+					<div class="col-md-9">
+						<div class="row">
+							<ul class="nav nav-tabs">
+							  <li class="active"><a href="<?php echo site_url('anggota/profile') ;?>">Overview</a></li>
+							  <li><a href="<?php echo site_url('anggota/riwayat_pendidikan') ;?>">Pendidikan</a></li>
+							  <li ><a href="<?php echo site_url('anggota/riwayat_organisasi') ;?>">Organisasi</a></li>
+							  <li ><a href="<?php echo site_url('anggota/riwayat_prestasi') ;?>">Prestasi</a></li>
+							  <li ><a href="<?php echo site_url('anggota/riwayat_kepanitiaan') ;?>">Kepanitiaan</a></li>
+							  <li ><a href="<?php echo site_url('anggota/riwayat_pelatihan') ;?>">Pelatihan</a></li>
+							   <li ><a href="<?php echo site_url('anggota/riwayat_pkm') ;?>">PKM</a></li>
+							</ul>
+						</div>
+					</div>
