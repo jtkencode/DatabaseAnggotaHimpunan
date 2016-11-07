4baf943aee28492dcbfc95366cbfc7af4360cf79
Imam Fauzan <imamfzn@gmail.com>
2016-11-07 23:18:38 +0700

    add edit button

--- application/views/anggota/riwayat/organisasi/content.php
+++ application/views/anggota/riwayat/organisasi/content.php
@@ -12,6 +12,11 @@
 										<div class="panel-body">
 											<p><?php echo $row->NAMA_ORG;?><br><small><?php echo $row->JABATAN_ORG;?></small></p>
 											<p class="text-right"><span class="glyphicon glyphicon-time"></span><small> <?php echo $row->TAHUN_MULAI_ORG.'-'.$row->TAHUN_SELESAI_ORG;?></small></p>
+											<!-- Button for edit riwayat-->
+											<div class="hiddenButton" hidden>
+												<a href="<?php echo site_url('anggota/riwayat_organisasi/update'),'/',$row->NO_URUT_ORG;?>" class="btn btn-success btn-sm" title="ubah data"><span class="glyphicon glyphicon-pencil"></span></a>
+												<a href="<?php echo site_url('anggota/riwayat_organisasi/delete'),'/',$row->NO_URUT_ORG;?>" class="btn btn-danger btn-sm" title="hapus data"><span class="glyphicon glyphicon-trash"></span></a>
+											</div>
 										</div>
 									</div>
 								</div>
