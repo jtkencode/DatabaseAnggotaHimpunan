c7a9cd9f8e17c2dc94d3a62ac3446d41f1da6949
Imam Fauzan <imamfzn@gmail.com>
2016-11-07 19:37:02 +0700

    change url riwayat organisasi on view

--- application/views/Anggota/index.php
+++ application/views/Anggota/index.php
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<ul class="nav nav-tabs">
 							  <li class="active"><a href="<?php echo site_url('anggota/index') ;?>">Overview</a></li>
 							  <li><a href="<?php echo site_url('anggota/riwayat_pendidikan') ;?>">Pendidikan</a></li>
-							  <li ><a href="<?php echo site_url('anggota/riwayat_organisasi') ;?>">Organisasi</a></li>
+							  <li ><a href="<?php echo site_url('anggota/riwayat/organisasi') ;?>">Organisasi</a></li>
 							  <li ><a href="<?php echo site_url('anggota/riwayat_prestasi') ;?>">Prestasi</a></li>
 							  <li ><a href="<?php echo site_url('anggota/riwayat_kepanitiaan') ;?>">Kepanitiaan</a></li>
 							  <li ><a href="<?php echo site_url('anggota/riwayat_pelatihan') ;?>">Pelatihan</a></li>
