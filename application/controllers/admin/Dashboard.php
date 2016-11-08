bf07af3b36e108deb78678aea514bc5f68433b59
Imam Fauzan <imamfzn@gmail.com>
2016-11-08 13:20:26 +0700

    change controller name

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -1,13 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin extends Admin_Controller {
+class Dashboard extends Admin_Controller {
 
 	public function __construct()
 	{
-
 		parent::__construct();
-
 		$this->load->helper('html');
 		$this->load->helper('url');
 		$this->load->model('Anggota_Model');
