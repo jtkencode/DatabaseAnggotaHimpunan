e4074471089bfaeecb99f9b0e947743513300f2e
Imam Fauzan <imamfzn@gmail.com>
2016-11-10 14:00:11 +0700

    change header font size from bootswatchpaper

--- application/views/anggota/header.php
+++ application/views/anggota/header.php
@@ -9,7 +9,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Data Anggota Himakom</title>
-	<link href="<?php echo base_url('assets/css/bootstrap-theme.min.css'); ?>" rel="stylesheet"> 
+	<link href="<?php echo base_url('assets/css/bootstrap-theme.min.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet"> 
 	<link href="<?php echo base_url('assets/css/timeline.css'); ?>" rel="stylesheet"> 
 	<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
 	<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
