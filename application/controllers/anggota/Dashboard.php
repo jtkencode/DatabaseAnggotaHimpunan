492c0f40d0dc855e78709d15c9604a074a67409f
Imam Fauzan <imamfzn@gmail.com>
2016-11-11 19:07:48 +0700

    add time function to detect days

--- application/controllers/anggota/Dashboard.php
+++ application/controllers/anggota/Dashboard.php
@@ -13,9 +13,21 @@ class Dashboard extends Anggota_Controller {
 	public function index()
 	{
 		$nim = $this->session->userdata('user');
+		$data['anggota'] = $this->Anggota_Model->get_id($nim);
 		$data['birthday'] = $this->Anggota_Model->get_birthday_of_week();
-		var_dump($data['birthday']);
-		$this->load->view('dashboard.html');
+ 		$data['waktu'] = $this->get_time_condition();
+		$this->load->view('dashboard.html',$data);
+	}
+
+	private function get_time_condition(){
+		date_default_timezone_set("Asia/Jakarta");
+		$time = localtime(time(), true);
+		$time = $time['tm_hour'];
+		if ($time >= 0 && $time < 12){
+			return "Pagi";
+		} else if ($time >= 12 && $time < 18){
+			return "Siang";
+		} return "Malam";
 	}
 
 	
