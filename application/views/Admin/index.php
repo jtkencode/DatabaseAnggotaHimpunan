f29a5b75cddaaafcdba8397d5acf1aedbadf8549
Imam Fauzan <imamfzn@gmail.com>
2016-11-11 15:04:54 +0700

    change bootstrap src

--- application/views/Admin/index.php
+++ application/views/Admin/index.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
 <head>
 	<title>Admin Dashboard</title>
-	<link href="<?php echo base_url('assets/css/bootstrap-theme.min.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet"> 
 	<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
 </head>
