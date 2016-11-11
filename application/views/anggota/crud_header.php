08d996c2f02d56a7a41a16c8e0dd835830bd5524
Imam Fauzan <imamfzn@gmail.com>
2016-11-11 14:55:47 +0700

    remove header

--- application/views/anggota/crud_header.php
+++ application/views/anggota/crud_header.php
@@ -1,14 +1,3 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-
-<html>
-<head>
-	<title>Data Anggota Himakom</title>
-	<link href="<?php echo base_url('assets/css/bootstrap-theme.min.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet"> 
-	<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
-</head>
 <body>
 	<div class="container">
 		<div class="page-header">
