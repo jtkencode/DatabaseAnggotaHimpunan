0ee71817ee83797c741278be403369dd94beb8f7
Imam Fauzan <imamfzn@gmail.com>
2016-11-12 15:38:06 +0700

    modify success view

--- application/views/Anggota/success.php
+++ application/views/Anggota/success.php
@@ -4,14 +4,19 @@
 			</div>
 			<div class="row">
 				<div class="col-sm-offset-3 col-sm-6">
+					<div class="alert alert-dismissible alert-success">
+					  	<button type="button" class="close" data-dismiss="alert">&times;</button>
+					  	<strong>Data berhasil disimpan !</strong> Terima kasih telah berkontribusi untuk Himakom:)
+					</div>
+
 					<div class="alert alert-success" role="alert">
-						<h2 align="center"><strong>Data berhasil disimpan!</strong></h2>
-						<h4 align="center">Terima kasih telah berkontribusi untuk Himakom:)</h4>
-						Silahkan memeriksa data yang sudah masuk pada <a href="<?php echo site_url('anggota');?>">dashboard anda</a> untuk memastikan data yang kamu masukkan sesuai. Mohon maaf atas ketidaknyamanan sistem kami yang masih belum sempurna :). <br><br>
+						Silahkan memeriksa data yang sudah masuk pada <a href="<?php echo site_url('anggota/profile');?>">dashboard anda</a> untuk memastikan data yang kamu masukkan sesuai. Mohon maaf atas ketidaknyamanan sistem kami yang masih belum sempurna :). <br><br>
 						<div class="text-center"> 
 							<a href="<?php echo site_url($path);?>" class="btn btn-success" align="center">Kembali ke Dashboard </a>
 						</div>
 					</div>
+
+					
 				</div>
 			</div><!-- end row -->		
 	</div>
