b555dcee185547ef4655c0f5bd681e24b36e2ccc
Imam Fauzan <imamfzn@gmail.com>
2016-11-16 13:21:57 +0700

    chang nim and id

--- application/models/Riwayat_Pelatihan_model.php
+++ application/models/Riwayat_Pelatihan_model.php
@@ -8,18 +8,18 @@ class Riwayat_Pelatihan_model extends CI_Model{
 		parent::__construct();
 	}
 
-	public function get_nim($nim)
+	public function get_id($id)
 	{
-		$query = $this->db->where('nim', $nim)->get('riwayat_pelatihan');
+		$query = $this->db->where('id_anggota', $id)->get('riwayat_pelatihan');
 		$result = $query->result();
 
 		return $result;
 	}
 
-	public function get_id($nim,$id)
+	public function get_no_urut($id,$no_urut)
 	{
-		$this->db->where('nim',$nim);
-		$this->db->where('no_urut_pelatihan', $id);
+		$this->db->where('id_anggota',$id);
+		$this->db->where('no_urut_pelatihan', $no_urut);
 		$query = $this->db->get('riwayat_pelatihan');
 		$result = $query->result();
 
@@ -29,7 +29,7 @@ class Riwayat_Pelatihan_model extends CI_Model{
 	public function get_last_no($id)
 	{
 		 
-		$this->db->where('nim', $id);
+		$this->db->where('id_anggota', $id);
 		$this->db->order_by('no_urut_pelatihan', 'desc');
 		$query = $this->db->limit(1)->get('riwayat_pelatihan');;
 		$result = $query->result();
@@ -41,11 +41,11 @@ class Riwayat_Pelatihan_model extends CI_Model{
 		return $result[0]->no_urut_pelatihan;
 	}
 
-	public function add_riwayat_pelatihan($nim)
+	public function add_riwayat_pelatihan($id)
 	{	
-		$no_urut = $this->get_last_no($nim) + 1; //append no
+		$no_urut = $this->get_last_no($id) + 1; //append no
 		$data = array(
-				'nim' => $nim,
+				'id_anggota' => $id,
 				'no_urut_pelatihan' => $no_urut,
 				'nama_pelatihan' => $this->input->post('nama_pelatihan'),
 				'nama_penyelenggara_pelatihan' => $this->input->post('nama_penyelenggara_pelatihan'),
@@ -57,7 +57,7 @@ class Riwayat_Pelatihan_model extends CI_Model{
 		return $query;
 	}
 
-	public function update_riwayat_pelatihan($nim,$id)
+	public function update_riwayat_pelatihan($id,$no_urut)
 	{		
 		$data = array(
 				'nama_pelatihan' => $this->input->post('nama_pelatihan'),
@@ -66,16 +66,16 @@ class Riwayat_Pelatihan_model extends CI_Model{
 				'pelatihan_kemahasiswaan' => $this->input->post('pelatihan_kemahasiswaan')
 			);
 
-		$this->db->where('nim',$nim);
-		$this->db->where('no_urut_pelatihan',$id);
+		$this->db->where('id_anggota',$id);
+		$this->db->where('no_urut_pelatihan',$no_urut);
 		$query = $this->db->update('riwayat_pelatihan',$data);
 		return $query;
 	}
 
-	public function delete_riwayat_pelatihan($nim,$id)
+	public function delete_riwayat_pelatihan($id,$no_urut)
 	{
-		$this->db->where('nim',$nim);
-		$this->db->where('no_urut_pelatihan',$id);
+		$this->db->where('id_anggota',$id);
+		$this->db->where('no_urut_pelatihan',$no_urut);
 		$query = $this->db->delete('riwayat_pelatihan');
 		return $query;
 	}
