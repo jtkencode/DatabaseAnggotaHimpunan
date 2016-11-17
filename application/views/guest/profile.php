b94ae0e6d143ac27cc17e5ad9badea482f7258a3
Imam Fauzan <imamfzn@gmail.com>
2016-11-17 08:21:22 +0700

    lowercase anddelete

--- application/views/guest/profile.php
+++ application/views/guest/profile.php
@@ -9,12 +9,12 @@
 							<h3><strong><?php echo $anggota->nama_lengkap;?></strong></h3>
 							<h4><?php echo $anggota->nama_bagus ;?></h4>
 							<p>Nama Departemen</p>
-							<p><?php echo $prodi[$anggota->id_ps]," ",$anggota->nama_kelas," ",$anggota->angkatan_kelas ;?></p>
+							<p>Kelas, Angkatan</p>
 							<hr>
 						</div>
 
 						<div>
-							<p><span class="glyphicon glyphicon-gift"></span> <?php echo $anggota->tempat_lahir.", ".date_format(date_create($anggota->TANGGAL_LAHIR), 'd F Y') ;?></p>
+							<p><span class="glyphicon glyphicon-gift"></span> <?php echo $anggota->tempat_lahir.", ".date_format(date_create($anggota->tanggal_lahir), 'd F Y') ;?></p>
 							<p><span class="glyphicon glyphicon-home"></span> <?php echo $anggota->alamat_sekarang ;?> </p>						
 							<?php foreach ($kontak as $row) : ?>
 								<p><span class="<?php echo $glyphicon[$row->JENIS_KONTAK];?>" ></span> <?php echo $row->detil_kontak;?></p>
