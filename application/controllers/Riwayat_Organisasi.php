150ef4191467682bbc6b758b60e9e54be67329d9
Imam Fauzan <imamfzn@gmail.com>
2016-11-17 08:25:59 +0700

    add id

--- application/controllers/Riwayat_Organisasi.php
+++ application/controllers/Riwayat_Organisasi.php
@@ -22,11 +22,10 @@ class Riwayat_Organisasi extends My_Controller {
 			$this->load->view('errors');
 		}
 		else {
-			$data['anggota'] = $this->Anggota_Model->get_id($nim);
-			$data['prodi']['D3-TI'] = "DIII-Teknik Informatika";
-			$data['prodi']['D4-TI'] = "Sarjana Terapan Teknik Informatika";
-			$data['kontak'] = $this->Kontak_model->get_id($nim);
-			$data['riwayat_org'] = $this->Riwayat_Org_model->get_nim($nim);
+			$data['anggota'] = $this->Anggota_Model->get_nim($nim);
+			$id = $data['anggota']->id_anggota;
+			$data['kontak'] = $this->Kontak_model->get_id($id);
+			$data['riwayat_org'] = $this->Riwayat_Org_model->get_id($id);
 			$ui['navtab']['page'] = 'organisasi';
 			$ui['navtab']['nim'] = $nim;
 			
