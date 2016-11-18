7cb07225a8e0193014b7241243cf6008a36f7053
Imam Fauzan <imamfzn@gmail.com>
2016-11-18 10:54:14 +0700

    to lowercase

--- application/views/anggota/dashboard.php
+++ application/views/anggota/dashboard.php
@@ -23,7 +23,7 @@
 								<?php date_default_timezone_set("Asia/Jakarta");$now = localtime(time(), true);?>
 								<?php foreach($birthday as $row): ?>
 									<?php $date = strtotime($row->tanggal); $count_down = (date('d',$date) - $now['tm_mday']); $date = $count_down == 0 ? "Hari ini <span class='glyphicon glyphicon-gift'></span>" : $count_down." hari lagi"; ?>
-									<p><a href="<?php echo site_url('profile/view/'.$row->NIM);?>"><?php echo $row->nama_lengkap;?> </a><small><?php echo $date;?></small></p>
+									<p><a href="<?php echo site_url('profile/view/'.$row->nim);?>"><?php echo $row->nama_lengkap;?> </a><small><?php echo $date;?></small></p>
 								<?php endforeach ; ?>
 							<?php endif; ?>
 						</div>						
