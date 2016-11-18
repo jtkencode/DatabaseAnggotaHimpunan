763836f89151550b844de8594445385a6a1d7158
Imam Fauzan <imamfzn@gmail.com>
2016-11-18 11:11:13 +0700

    initiate kontak controller

---
+++ application/controllers/anggota/Kontak.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kontak extends Anggota_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();				
+		$this->load->model('Anggota_Model');
+	
+	}
+
+	public function index()
+	{
+		$id = $this->session->userdata('user_id');
+		$data['anggota'] = $this->Anggota_Model->get_id($id);
+		$data['birthday'] = $this->Anggota_Model->get_birthday_of_week();
+ 		$data['waktu'] = $this->get_time_condition();
+ 		$ui['nama_anggota'] = $data['anggota']->nama_lengkap;
+ 		$this->load->view('anggota/header');
+ 		$this->load->view('anggota/body',$ui);
+		$this->load->view('anggota/dashboard',$data);
+		$this->load->view('anggota/footer');
+
+	}
+
+	private function get_time_condition(){
+		date_default_timezone_set("Asia/Jakarta");
+		$time = localtime(time(), true);
+		$time = $time['tm_hour'];
+		if ($time >= 0 && $time < 12){
+			return "Pagi";
+		} else if ($time >= 12 && $time < 18){
+			return "Siang";
+		} return "Malam";
+	}
+
+	
+}
