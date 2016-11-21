f3f11f596624713a1368cc86410273c32f5ca4f3
Imam Fauzan <imamfzn@gmail.com>
2016-11-21 14:07:56 +0700

    add new controller for register

--- application/core/My_Controller.php
+++ application/core/My_Controller.php
@@ -8,6 +8,7 @@ class My_Controller extends CI_Controller {
 		parent::__construct();
 		$this->load->library('session');
 		$this->load->model('Anggota_Model');
+		$this->load->model('Kontak_model');
 
 	}		
 }
@@ -18,6 +19,10 @@ class Admin_Controller extends My_Controller {
 		parent::__construct();
 		if (!$this->identity->is_admin())
 			redirect('site/login');
+
+		if ($this->Anggota_Model->is_not_complete($id) || $this->Kontak_model->is_not_complete($id)){
+			redirect('anggota/register');
+		}
 	}		
 }
 
@@ -27,6 +32,22 @@ class Anggota_Controller extends My_Controller {
 		parent::__construct();
 		if (!$this->identity->is_anggota())
 			redirect('site/login');
+
+		if ($this->Anggota_Model->is_not_complete($id) || $this->Kontak_model->is_not_complete($id)){
+			redirect('anggota/register');
+		}
 	}
 }
+
+class Register_Controller extends My_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Anggota_Model');
+		if (!$this->identity->is_anggota())
+			redirect('site/login');
+	}
+}
+
 ?>
