774f762cd4261ba027d8f125d7dfb9ba7e6f21d1
Imam Fauzan <imamfzn@gmail.com>
2016-11-21 15:44:52 +0700

    add condition

--- application/views/Anggota/Register/add_contact.php
+++ application/views/Anggota/Register/add_contact.php
@@ -8,26 +8,32 @@
 		<?php endif; ?>
 			<div class="col-md-9 col-md-offset-1">
 				<form class="form-horizontal" id="formRiwayatOrganisasi" method="post">
+				<?php if (!$has_phone_number) : ?>
 					<div class="form-group">
 						<label class="col-md-3 control-label" for="inputNoHp">No. Hp</label>
 						<div class="col-md-9">
 							<input name="no_hp" class="form-control" id="inputNoHp" required="" type="text" placeholder="No Handphone" value="" autocomplete="off">
 						</div>
 					</div>
+				<?php endif; ?>
 
+				<?php if (!$has_polban_email) : ?>
 					<div class="form-group">
 						<label class="col-md-3 control-label" for="inputEmailPolban">Email Polban</label>
 						<div class="col-md-9">
 							<input name="email_polban" class="form-control" id="inputEmailPolban" required="" type="text" placeholder="Alamat Email Polban" value="" autocomplete="off">
 						</div>
 					</div>
+				<?php endif; ?>
 
+				<?php if (!$has_private_email) : ?>
 					<div class="form-group">
 						<label class="col-md-3 control-label" for="inputEmailPribadi">Email Pribadi</label>
 						<div class="col-md-9">
 							<input name="email_pribadi" class="form-control" id="inputEmailPribadi" required="" type="text" placeholder="Alamat Email Pribadi" value="" autocomplete="off">
 						</div>
 					</div>
+				<?php endif; ?>
 
 					<div class="form-group">
 						<div class="col-md-6 col-md-offset-3">
