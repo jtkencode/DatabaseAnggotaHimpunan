cfe5a4b45f44f9eae81102259d1bf36b8a550400
Imam Fauzan <imamfzn@gmail.com>
2016-11-22 16:40:21 +0700

    change login style

--- application/views/login.php
+++ application/views/login.php
@@ -11,17 +11,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<link href="<?php echo base_url('assets/js/bootstrap.min.js'); ?>" rel="stylesheet"> 
 		<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
 	</head>
-	<body>
+	<body class="cover">
+		<div class="page-header no-line-header">
+		</div>
 		<div class="container">
-			<div class="page-header">
-				<h1>Login Dashboard</h1>
-			</div>
 			<div class="row">
 				<!-- start col login -->
 				<div class="col-sm-offset-4 col-sm-4">
-					<div class="panel panel-default">
+					<div class="panel panel-default panel-no-background">
 						<div class="panel-heading text-center">
-							<img src="<?php echo base_url('assets/img/logo_himakom.png'); ?>" style="max-width: 75%;">
+							<img src="<?php echo base_url('assets/img/logo_himakom.png'); ?>" style="max-width: 60%;">
 						</div>
 						<div class="panel-body">
 							<?php if (isset($error)) :?>
@@ -32,21 +31,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<?php endif; ?>
 							<form class="form-horizontal" action="<?php echo site_url('site/login')?>" method="POST">
 								<div class="form-group">
-									<label for="inputUsername" class="col-sm-4 control-label">Username</label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control" name="username" id="inputUsername" 
+									
+									<div class="col-sm-12">
+										<input type="text" class="form-control input-lg login-input" name="username" id="inputUsername" 
 											placeholder="Masukkan username" required autofocus>
 									</div>
 								</div>
 								<div class="form-group">
-									<label for="inputPassword" class="col-sm-4 control-label">Password</label>
-									<div class="col-sm-8">
-										<input type="password" class="form-control" name="password" id="inputPassword" 
+									
+									<div class="col-sm-12">
+										<input type="password" class="form-control input-lg login-input" name="password" id="inputPassword" 
 											placeholder="Masukkan password" required>
 									</div>
 								</div>
 								<div class="form-group">
-									<div class="col-sm-offset-4 col-sm-8">
+									<div class="col-sm-12">
 										<button type="submit" class="btn btn-primary btn-block">Login</button>
 									</div>
 								</div>
