d5853e22e35231abf4f6591587275e9892ee4e2f
Imam Fauzan <imamfzn@gmail.com>
2016-11-22 10:13:39 +0700

    add condition

--- application/views/anggota/riwayat/pkm/content.php
+++ application/views/anggota/riwayat/pkm/content.php
@@ -1,18 +1,18 @@
 					<!--Riwayat PKM -->
 					<div class="col-md-9">
+					<?php if (count($riwayat_pkm) == 0):?>
 						<div class="row">
 							<div class="col-md-12">
-								<h3><small>Pengabdian Kepada Masyarakat yang pernah diikuti</small></h3>
-								<?php if (count($riwayat_pkm)==0) :?>
-									<div class="panel panel-default">
-										<div class="panel-body">
-											<p>Belum Pernah Mengikuti PKM.......</p>
-										</div>
-									</div>
-								<?php endif;?>
+								<h3 class="text-center"><small>Belum pernah mengikuti kegiatan pengabdian kepada masyarakat</small></h3>
 							</div>
 						</div>
-
+					<?php else: ?>
+						<div class="row">
+							<div class="col-md-12">
+								<h3><small>Kegiatan PKM yang pernah diikuti</small></h3>
+							</div>
+						</div>
+					<?php endif; ?>
 						<div class="row">
 							<?php foreach ($riwayat_pkm as $row) :?>
 								<div class="col-md-6">
