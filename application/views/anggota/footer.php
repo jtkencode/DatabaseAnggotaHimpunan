01d379a82e624798dce303cc89bc5f59fa13412d
Imam Fauzan <imamfzn@gmail.com>
2016-12-17 12:06:18 +0700

    add comment

--- application/views/anggota/footer.php
+++ application/views/anggota/footer.php
@@ -1,6 +1,6 @@
 				</div>
 				<footer>
-					<hr> <p class="text-center">&copy; 2016 Himpunan Mahasiswa Komputer Politeknik Negeri Bandung</p>
+					<hr> <p class="text-center">&copy; 2016 <!--IF,MS-->Himpunan Mahasiswa Komputer Politeknik Negeri Bandung</p>
 				</footer>
 			</div>	
 		</div>		
