3af9e4414380dac90ce7900b085f1d88d6c3f309
Imam Fauzan <imamfzn@gmail.com>
2016-12-17 12:06:27 +0700

    add responsive

--- application/views/login.php
+++ application/views/login.php
@@ -5,11 +5,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Dashboard Login</title>
 		<link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet"> 
 		<link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
-		<link href="<?php echo base_url('assets/js/bootstrap.min.js'); ?>" rel="stylesheet"> 
 		<script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
+		<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 	</head>
 	<body class="cover">
 		<div class="page-header no-line-header">
